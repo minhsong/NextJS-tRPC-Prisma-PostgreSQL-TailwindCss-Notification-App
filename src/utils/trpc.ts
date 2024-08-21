3da9a1a5ee707e428499ac9710c7a4a2bbc6d48d
Minh Song <truongminhsong@gmail.com>
2024-08-21 12:35:56 +0700

    commit for rebuild

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -10,7 +10,8 @@ function getBaseUrl() {
   if (process.env.VERCEL_URL)
     // reference for vercel.com
     return `https://${process.env.VERCEL_URL}`;
-  // assume localhost
+
+  //default assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
